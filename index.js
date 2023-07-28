3daf93249a786eb1df52361b60512b696fd2a991
AHMAD <aua321890@gmail.com>
2023-07-28 10:06:27 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const helmet = require("helmet");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+// const fileupload = require("express-fileupload");
+// router
+const userRouter = require("./src/router/user.router");
+const foodRouter = require("./src/router/food.router");
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(helmet());
+// app.use(
+//   helmet({
+//     crossOriginEmbedderPolicy: false,
+//     // ...
+//   })
+// );
+// app.use(fileupload());
+
+app.use(userRouter);
+app.use(foodRouter);
+// app.use(express.static("public"));
+
+app.listen(4000, () => {
+  console.log("SERVER LISTEN ON PORT 4000");
+});
